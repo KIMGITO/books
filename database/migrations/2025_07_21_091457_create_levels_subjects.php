4c2c0bca6d7418787ce5fcd99ef9ef53f56a9d8e
KIMGITO <kimanthidennis02@gmail.com>
2025-07-21 12:16:46 +0300

    feat: level_subject table

---
+++ database/migrations/2025_07_21_091457_create_levels_subjects.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('levels_subjects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('level_id')->constrained('levels')->onDelete('cascade');
+            $table->foreignId('subject_id')->constrained('subjects')->onDelete('cascade');
+            $table->foreignId('created_by')->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->constrained('users')->onDelete('set null');
+            $table->unique(['level_id', 'subject_id'], 'unique_level_subject');
+            $table->index(['created_by', 'updated_by'], 'levels_subjects_user_index');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('levels_subjects');
+    }
+};
