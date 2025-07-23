441a00bcd77b00ba20fafe3b1ba9a912db57be93
KIMGITO <kimanthidennis02@gmail.com>
2025-07-23 18:49:24 +0300

    fix:scroling bar for teachers table

--- resources/js/pages/dash/teachers.tsx
+++ resources/js/pages/dash/teachers.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import AppLayout from "@/layouts/app-layout";
 
@@ -8,21 +9,21 @@ export default function Teachers
             <div className="p-4">
                 <h1 className="text-2xl font-bold">Teachers</h1>
                 <p className="mt-2 text-gray-600">Manage teacher records and details.</p>
-                <div className="flex w-full flex-col items-center justify-center">
-                    <div className="rounded-2xl border p-2 w-11/12  md:w-fit">
-                        <Table>
-                            <TableHeader>
-                                <TableHead>#</TableHead>
-                                <TableHead>Name</TableHead>
-                                <TableHead>Department</TableHead>
-                                <TableHead>Subjects</TableHead>
-                                <TableHead>Books issued</TableHead>
-                                <TableHead>Role</TableHead>
-                                <TableHead className="text-end">Actions</TableHead>
-                            </TableHeader>
-                            <TableBody>
-                                {
-                                    Array.from({ length: 5 }).map(() => (
+                <div className="flex w-full flex-col items-center justify-center  mt-6">
+                    <div className="w-11/12 rounded-2xl border p-2 md:w-fit">
+                        <ScrollArea>
+                            <Table>
+                                <TableHeader>
+                                    <TableHead>#</TableHead>
+                                    <TableHead>Name</TableHead>
+                                    <TableHead>Department</TableHead>
+                                    <TableHead>Subjects</TableHead>
+                                    <TableHead>Books issued</TableHead>
+                                    <TableHead>Role</TableHead>
+                                    <TableHead className="text-end">Actions</TableHead>
+                                </TableHeader>
+                                <TableBody>
+                                    {Array.from({ length: 5 }).map(() => (
                                         <TableRow className="text-center">
                                             <TableCell>1</TableCell>
                                             <TableCell>John Doe</TableCell>
@@ -32,13 +33,13 @@ export default function Teachers
                                             <TableCell>Teacher</TableCell>
                                             <TableCell className="text-right">
                                                 <button className="ml-2 text-green-500 hover:underline">More</button>
-                                                </TableCell>
+                                            </TableCell>
                                         </TableRow>
-                                    ))
-
-                                }
-                            </TableBody>
-                        </Table>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                            <ScrollBar orientation="horizontal"/>
+                        </ScrollArea>
                     </div>
                 </div>
             </div>
