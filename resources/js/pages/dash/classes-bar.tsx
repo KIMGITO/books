db64277a872a5ea0cbd1d1401b268a2b0f9e0c63
KIMGITO <kimanthidennis02@gmail.com>
2025-07-23 18:48:41 +0300

    feat add a class selector bar in student table, fixed the scrolling area to make it ui good

---
+++ resources/js/pages/dash/classes-bar.tsx
@@ -0,0 +1,12 @@
+export default function ClassesBar() {
+    return (
+        <div className="mt-2 mb-6 w-full ">
+            <div className="grid grid-cols-10 gap-2">
+                <div className="rounded bg-gray-400 px-2 text-center">F1</div>
+                <div className="rounded bg-gray-400 px-2 text-center">F2</div>
+                <div className="border-rounded border-b-gray-400 border-b bg-gray-400/30 px-2 text-center">F3 </div>
+                <div className="rounded bg-gray-400 px-2 text-center">F4 </div>
+            </div>
+        </div>
+    );
+}
--- resources/js/pages/dash/students.tsx
+++ resources/js/pages/dash/students.tsx
@@ -1,5 +1,7 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/app-layout';
+import ClassesBar from './classes-bar';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 
 export default function Students() {
     return (
@@ -7,34 +9,39 @@ export default function Students() {
             <div className="p-4">
                 <h1 className="text-2xl font-bold">Students</h1>
                 <p className="mt-2 text-gray-600">View And Manage Student records and details.</p>
-                <div className="flex flex-col  items-center justify-center">
-                    <div className="rounded-2xl w-11/12 border p-2  md:w-fit">
-                        <Table className='w-1/2'>
-                            <TableHeader>
-                                <TableHead>#</TableHead>
-                                <TableHead>ADM.No</TableHead>
-                                <TableHead>Name</TableHead>
-                                <TableHead>Class</TableHead>
-                                <TableHead>Subjects Taken</TableHead>
-                                <TableHead>Books Owed</TableHead>
-                                <TableHead className="text-end">Actions</TableHead>
-                            </TableHeader>
-                            <TableBody>
-                                {Array.from({ length: 5 }).map(() => (
-                                    <TableRow className="text-center">
-                                        <TableCell>1</TableCell>
-                                        <TableCell>1161</TableCell>
-                                        <TableCell>dennis kimanthi</TableCell>
-                                        <TableCell>form 4</TableCell>
-                                        <TableCell className=''>english, maths, chemistry, geography, physics</TableCell>
-                                        <TableCell>10</TableCell>
-                                        <TableCell className="text-right">
-                                            <button className="ml-2 text-green-500 hover:underline">More</button>
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
+                <div className="mt-6 flex flex-col items-center justify-center">
+                    <div className="w-11/12 rounded-2xl border p-2 md:w-fit">
+                        <ClassesBar />
+                        <div className="w-full border-t border-t-gray-600"></div>
+                        <ScrollArea>
+                            <Table className="w-1/2 py-2">
+                                <TableHeader>
+                                    <TableHead>#</TableHead>
+                                    <TableHead>ADM.No</TableHead>
+                                    <TableHead>Name</TableHead>
+                                    <TableHead>Class</TableHead>
+                                    <TableHead>Subjects Taken</TableHead>
+                                    <TableHead>Books Owed</TableHead>
+                                    <TableHead className="text-end">Actions</TableHead>
+                                </TableHeader>
+                                <TableBody>
+                                    {Array.from({ length: 5 }).map(() => (
+                                        <TableRow className="text-center">
+                                            <TableCell>1</TableCell>
+                                            <TableCell>1161</TableCell>
+                                            <TableCell>dennis kimanthi</TableCell>
+                                            <TableCell>form 4</TableCell>
+                                            <TableCell className="">english, maths, chemistry, geography, physics</TableCell>
+                                            <TableCell>10</TableCell>
+                                            <TableCell className="text-right">
+                                                <button className="ml-2 text-green-500 hover:underline">More</button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                            <ScrollBar orientation='horizontal' className='mt-6' />
+                        </ScrollArea>
                     </div>
                 </div>
             </div>
