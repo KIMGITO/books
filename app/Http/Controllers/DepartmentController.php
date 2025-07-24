92e3c5e4fe92922d3b8405b3390e1b97d1777df6
KIMGITO <kimanthidennis02@gmail.com>
2025-07-24 13:30:45 +0300

    feat:department store method

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Models\Department;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreDepartmentRequest;
 
 class DepartmentController extends Controller
 {
@@ -13,7 +15,12 @@ class DepartmentController extends Controller
         ]);
     }
 
-    public function store(){
+    public function store(StoreDepartmentRequest $request){
+        $validated = $request->validated();
+
+        Department::create($validated);
+
+        return redirect()->route('departments.index')->with(['success' => 'Department created successfully.']);
 
     }
 }
